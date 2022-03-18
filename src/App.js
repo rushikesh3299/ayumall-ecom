6810bb98cef200999cc6de6433fe239adcbc6d6e
unknown <rushikeshchougule3299@gmail.com>
2022-03-18 18:37:54 +0530

    Basic router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import Login from "./pages/authpages/Login";
+import Signup from "./pages/authpages/Signup";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
