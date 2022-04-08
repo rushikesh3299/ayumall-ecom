b591ec307407200235eab4c89e3c507e6a85d118
rushikesh3299 <rushikeshchougule3299@gmail.com>
2022-04-08 08:15:58 +0530

    [Feat] Filter Products

---
+++ src/reducer/product-reducer.js
@@ -0,0 +1,49 @@
+import { useReducer } from "react";
+
+export const productReducer = () => {
+  const filterHandler = (productState, action) => {
+    switch (action.type) {
+      case "SORT": {
+        return action.payload === "HIGH_TO_LOW"
+          ? { ...productState, sortBy: "HIGH_TO_LOW" }
+          : { ...productState, sortBy: "LOW_TO_HIGH" };
+      }
+      case "CATEGORIZE": {
+        return productState.showCategory.includes(action.payload)
+          ? {
+              ...productState,
+              showCategory: [
+                ...productState.showCategory.filter(
+                  (item) => item !== action.payload
+                ),
+              ],
+            }
+          : {
+              ...productState,
+              showCategory: [...productState.showCategory, action.payload],
+            };
+      }
+      case "RATE": {
+        return { ...productState, showRating: action.payload };
+      }
+      case "CLEAR_ALL": {
+        return {
+          ...productState,
+          sortBy: null,
+          showCategory: [],
+          showRating: 0,
+        };
+      }
+      default:
+        return { ...productState };
+    }
+  };
+
+  const initialState = { sortBy: null, showCategory: [], showRating: 0 };
+  const [productState, productDispatch] = useReducer(
+    filterHandler,
+    initialState
+  );
+
+  return { productState, productDispatch };
+};
