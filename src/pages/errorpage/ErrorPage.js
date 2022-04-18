a2a43f3c9b22cd7c05afecaa8eee22d4a6229c38
rushikesh3299 <rushikeshchougule3299@gmail.com>
2022-04-18 12:49:53 +0530

    css refacotor

--- src/pages/errorpage/ErrorPage.js
+++ src/pages/errorpage/ErrorPage.js
@@ -4,7 +4,7 @@ export const ErrorPage = () => {
   return (
     <div className="error-page-container taC p30">
       <div className="error-page-title fs40 fw900">404</div>
-      <div class="error-page-text fs25 fw600 p10">Page NOT Found</div>
+      <div className="error-page-text fs25 fw600 p10">Page NOT Found</div>
       <NavLink className="fw600" to="/">
         Return to HomePage
       </NavLink>
