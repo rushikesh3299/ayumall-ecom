45e381ad407f36c505394f6f228f4d6f0279b0d9
rushikesh3299 <rushikeshchougule3299@gmail.com>
2022-04-27 14:38:56 +0530

    refactor code
    move item from wishlist to cart
    minor css changes
    null wishlist handled
    null cart handled

--- src/pages/wishlistpage/Wishlist.js
+++ src/pages/wishlistpage/Wishlist.js
@@ -1,41 +1,58 @@
 import "./wishlist.css";
-import { useWishlist } from "../../context/index";
+import { useWishlist, useCart } from "../../context/index";
 
 export const Wishlist = () => {
   const { wishlistItems, removeItemFromWishlist } = useWishlist();
+  const { addToCart } = useCart();
   return (
     <div className="wishlist-container">
-      <div className="wishlist-title">
-        Your Wishlist <span>{wishlistItems.length} items</span>
-      </div>
+      {wishlistItems.length === 0 ? (
+        <div className="wishist-section-empty">
+          <h1>Your wishlist is empty</h1>
+        </div>
+      ) : (
+        <div>
+          <div className="wishlist-title">
+            Your Wishlist has <span>{wishlistItems.length} items</span>
+          </div>
 
-      <div className="wishlist-section">
-        {wishlistItems.map((item) => {
-          return (
-            <div className="product-card wishlist-card" key={item._id}>
-              <div
-                className="cart-item-remove"
-                onClick={() => removeItemFromWishlist(item._id)}
-              >
-                &#x2716;
-              </div>
-              <div className="product-card-img">
-                <img src={item.image} alt="" />
-              </div>
-              <div className="product-card-discription">
-                <div className="product-card-name">{item.title}</div>
-                <div className="product-card-brand">{item.brand}</div>
-                <div className="product-card-price">
-                  <div>{item.price}</div>
-                  <div>(10% OFF)</div>
+          <div className="wishlist-section">
+            {wishlistItems.map((item) => {
+              return (
+                <div className="product-card wishlist-card" key={item._id}>
+                  <div
+                    className="cart-item-remove"
+                    onClick={() => removeItemFromWishlist(item._id)}
+                  >
+                    &#x2716;
+                  </div>
+                  <div className="product-card-img">
+                    <img src={item.image} alt="" />
+                  </div>
+                  <div className="product-card-discription">
+                    <div className="product-card-name">{item.title}</div>
+                    <div className="product-card-brand">{item.brand}</div>
+                    <div className="product-card-price">
+                      <div>{item.price}</div>
+                      <div>(10% OFF)</div>
+                    </div>
+                    <div className="product-card-quantity">{item.weight}</div>
+                    <button
+                      className="product-card-btn-add"
+                      onClick={() => {
+                        removeItemFromWishlist(item._id);
+                        addToCart(item);
+                      }}
+                    >
+                      Move to cart
+                    </button>
+                  </div>
                 </div>
-                <div className="product-card-quantity">{item.weight}</div>
-                <button className="product-card-btn-add">Add to cart</button>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
